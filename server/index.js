910a0637a74f3fa03d3d0de9523d77bd647a73ea
alex <shizofranky@gmail.com>
2024-04-23 12:51:46 +0300

    refactor

--- server/index.js
+++ server/index.js
@@ -1,25 +1,28 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const MyMiddleWare = require("./my-middleware/authenticate_middleware.js");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const MyMiddleWare = require("./my-middleware/authenticate_middleware.js");
 
-let a = 0;
+// const { api_router } = require("./routing/router.js");
 
-const { api_router } = require("./routing/router.js");
-
-// const myMiddleware = require("./my-middleware");
-require("../database/db");
+// require("../database/db");
 const app = express();
 
-require("dotenv").config();
+// require("dotenv").config();
 
-console.log('the port: ' , process.env.port) ;
+// console.log('the port: ' , process.env.port) ;
 
-app.use(cors());
-// app.use(MyMiddleWare);
-app.use(bodyParser.json());
-app.use(express.static("uploads"));
-app.use("/api", api_router);
+// app.use(cors());
+
+// app.use(bodyParser.json());
+// app.use(express.static("uploads"));
+// app.use("/api", api_router);
+
+app.get('/' , (req , res ) => {
+  res.status(200).json({
+    message:'foo bar' ,
+  });
+});
 
 app.listen(3002, () => {
   console.log(`express run on port `);
