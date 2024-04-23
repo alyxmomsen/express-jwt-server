8c51915b96e4efc430dd6a79f89ceecdbb6e454a
alex <shizofranky@gmail.com>
2024-04-23 13:17:28 +0300

    refactor

--- server/index.js
+++ server/index.js
@@ -1,28 +1,22 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const MyMiddleWare = require("./my-middleware/authenticate_middleware.js");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MyMiddleWare = require("./my-middleware/authenticate_middleware.js");
 
-// const { api_router } = require("./routing/router.js");
+const { api_router } = require("./routing/router.js");
 
-// require("../database/db");
+require("../database/db");
 const app = express();
 
-// require("dotenv").config();
+require("dotenv").config();
 
-// console.log('the port: ' , process.env.port) ;
+console.log('the port: ' , process.env.port) ;
 
-// app.use(cors());
+app.use(cors());
 
-// app.use(bodyParser.json());
-// app.use(express.static("uploads"));
-// app.use("/api", api_router);
-
-app.get('/' , (req , res ) => {
-  res.status(200).json({
-    message:'foo bar' ,
-  });
-});
+app.use(bodyParser.json());
+app.use(express.static("uploads"));
+app.use("/api", api_router);
 
 app.listen(3002, () => {
   console.log(`express run on port `);
